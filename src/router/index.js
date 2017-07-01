771d0eee9fc56312302d2351d2b62b8b4aae36c8
Abdullah Mara <zull.mara00@gmail.com>
2017-07-01 23:02:13 +0300

    Fix scroll issue

--- src/router/index.js
+++ src/router/index.js
@@ -8,10 +8,18 @@ import Project from '../components/Project.vue';
 Vue.use(Router);
 
 export default new Router({
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  },
   routes: [
     { path: '', component: Home, name: 'Home' },
     { path: '/about', component: About, name: 'About' },
-    { path: '/project/:url', component: Project, name: 'Project' }
+    {
+      path: '/project/:url',
+      props: true,
+      component: Project,
+      name: 'Project'
+    }
   ],
   mode: 'history'
 });
